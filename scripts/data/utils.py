f2f08207763f62734b02e8fbecbf49b23353f661
Ento0n <antspa@gmx.de>
2025-10-08 15:16:54 +0200

    Add per sequence identities of the the test set to the normal test set

---
+++ scripts/data/utils.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+def add_labels(identity_df_path, origin_df_path):
+    identity_df = pd.read_csv(identity_df_path)
+    origin_df = pd.read_csv(origin_df_path)
+
+    # add sequence identity to origin_df
+    for idx, row in origin_df.iterrows():
+        receptor_seq = row["receptor_seq"]
+        ligand_seq = row["ligand_seq"]
+
+        mask = (identity_df["receptor_seq"] == receptor_seq) & (identity_df["ligand_seq"] == ligand_seq)
+        if mask.any():
+            origin_df.at[idx, "max_id_to_train_receptor"] = identity_df.loc[mask, "max_id_to_train_receptor"].values[0]
+            origin_df.at[idx, "max_id_to_train_ligand"] = identity_df.loc[mask, "max_id_to_train_ligand"].values[0]
+        else:
+            print(f"Could not find identity for {row['entry']}")
+    
+    origin_df.to_csv(os.path.join("/nfs/scratch/pinder/negative_dataset/my_repository/datasets/deleak_uniprot/deleak_cdhit", "test_with_identities.csv"), index=False)
+
+
+if __name__ == "__main__":
+    identity_df_path = "/nfs/scratch/pinder/negative_dataset/my_repository/datasets/deleak_uniprot/deleak_cdhit/test_with_identities_raw.csv"
+    origin_df_path = "/nfs/scratch/pinder/negative_dataset/my_repository/datasets/deleak_uniprot/deleak_cdhit/test.csv"
+
+    add_labels(identity_df_path, origin_df_path)
