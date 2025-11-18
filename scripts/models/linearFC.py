c3aa6366c011a590605e66e575782432e4146018
Ento0n <antspa@gmx.de>
2025-11-18 11:32:34 +0100

    Misc

--- scripts/models/linearFC.py
+++ scripts/models/linearFC.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 # 1. Define a simple feed-forward classifier
-class SimpleInteractionNet(nn.Module):
+class LinearFC(nn.Module):
     def __init__(self, input_dim):
         super().__init__()
         self.fc = nn.Sequential(
